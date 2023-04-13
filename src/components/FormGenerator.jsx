5fb5606e82474156bf8b69b84a8b72b4e4acdeff
LuckyFox <noah.grebet@orange.fr>
2023-04-13 09:44:58 +0200

    :sparkles: Collect data

--- src/components/FormGenerator.jsx
+++ src/components/FormGenerator.jsx
@@ -1,10 +1,40 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 export default function FormGenerator({template}){
 	const [data, setData] = useState([]);
 
+	useEffect(() => {
+		const jsonPreviewData = document.querySelector("#json-preview-data");
+		jsonPreviewData.value = JSON.stringify(data);
+	}, [data])
+
 	function formSubmitHandler(e){
 		e.preventDefault();
+	}
+
+	function inputChangeHandler(event){
+		const name = event.target.name.split('_')[0];
+
+		const index = data.findIndex((data) => {
+			return data.name === name
+		});
+
+		if(index !== -1){
+			const newDataArray = [...data];
+			newDataArray[index] = {
+				name: name,
+				value: event.target.value
+			}
+			setData(newDataArray);
+		} else{
+			setData([
+				...data,
+				{
+					name: name,
+					value: event.target.value
+				}
+			])
+		}
 	}
 
 	function generateRandomStringId(){
@@ -22,13 +52,15 @@ export default function FormGenerator({template}){
 	}
 
 	function inputRenderer(input, key){
+		const randomId = generateRandomStringId();
+
 		if(input.blockInput){
 			return (
 				<div key={key}>
 					<p className="input-label">{input.name}</p>
 					{
 						input.htmlInput === 'select' ? (
-							<select className="form-select-generator-input">
+							<select className="form-select-generator-input" name={replaceSpacesWithHyphens(input.name) + '_' + randomId} onChange={inputChangeHandler}>
 								{displayAllChildInputFromBlockInput(input)}
 							</select>
 						) : displayAllChildInputFromBlockInput(input)
@@ -37,45 +69,43 @@ export default function FormGenerator({template}){
 			)
 		}
 
-		const randomId = generateRandomStringId();
-
 		switch (input.htmlInput){
 			case 'textarea':
 				return (
 					<p className="input-container" key={key}>
-						<label htmlFor={replaceSpacesWithHyphens(input.name) + '-' + randomId}>{input.name}</label>
-						<textarea name={replaceSpacesWithHyphens(input.name) + '-' + randomId} id={replaceSpacesWithHyphens(input.name) + '-' + randomId} required={input.required}></textarea>
+						<label htmlFor={replaceSpacesWithHyphens(input.name) + '_' + randomId}>{input.name}</label>
+						<textarea name={replaceSpacesWithHyphens(input.name) + '_' + randomId} id={replaceSpacesWithHyphens(input.name) + '_' + randomId} required={input.required} onChange={inputChangeHandler}></textarea>
 					</p>
 				)
 
 			case 'checkbox':
 				return (
 					<p className="checkbox-container" key={key}>
-						<label htmlFor={replaceSpacesWithHyphens(input.name) + '-' + randomId}>{input.name}</label>
-						<input type="checkbox" id={replaceSpacesWithHyphens(input.name) + '-' + randomId} name={input.parent ? replaceSpacesWithHyphens(input.parent) : replaceSpacesWithHyphens(input.name) + '-' + randomId} required={input.required} />
+						<label htmlFor={replaceSpacesWithHyphens(input.name) + '_' + randomId}>{input.name}</label>
+						<input type="checkbox" value={replaceSpacesWithHyphens(input.name)} id={replaceSpacesWithHyphens(input.name) + '_' + randomId} name={input.parent ? replaceSpacesWithHyphens(input.parent) : replaceSpacesWithHyphens(input.name) + '_' + randomId} required={input.required} onChange={inputChangeHandler} />
 					</p>
 				)
 
 			case 'radio':
 				return (
 					<p className="radio-container" key={key}>
-						<label htmlFor={replaceSpacesWithHyphens(input.name) + '-' + randomId}>{input.name}</label>
-						<input type="radio" id={replaceSpacesWithHyphens(input.name) + '-' + randomId} name={input.parent ? replaceSpacesWithHyphens(input.parent) : replaceSpacesWithHyphens(input.name) + '-' + randomId} required={input.required} />
+						<label htmlFor={replaceSpacesWithHyphens(input.name) + '_' + randomId}>{input.name}</label>
+						<input type="radio" value={replaceSpacesWithHyphens(input.name)} id={replaceSpacesWithHyphens(input.name) + '_' + randomId} name={input.parent ? replaceSpacesWithHyphens(input.parent) : replaceSpacesWithHyphens(input.name) + '_' + randomId} required={input.required} onChange={inputChangeHandler} />
 					</p>
 				)
 
 			case 'select':
 				return (
-					<option key={key}>
-						<p className="input-label">{input.name}</p>
+					<option key={key} value={input.value}>
+						{input.name}
 					</option>
 				)
 
 			default:
 				return (
 					<p className="input-container" key={key}>
-						<label htmlFor={replaceSpacesWithHyphens(input.name) + '-' + randomId}>{input.name}</label>
-						<input type={input.htmlInput} name={replaceSpacesWithHyphens(input.name) + '-' + randomId} id={replaceSpacesWithHyphens(input.name) + '-' + randomId} required={input.required} />
+						<label htmlFor={replaceSpacesWithHyphens(input.name) + '_' + randomId}>{input.name}</label>
+						<input type={input.htmlInput} name={replaceSpacesWithHyphens(input.name) + '_' + randomId} id={replaceSpacesWithHyphens(input.name) + '_' + randomId} required={input.required} onChange={inputChangeHandler} />
 					</p>
 				)
 		}
